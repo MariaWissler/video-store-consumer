244a237ee01d98b78b4a97ea2dea9ebd541217fb
kateannnichols <kateannnichols@gmail.com>
2019-06-26 13:47:48 -0700

    working on customer view

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -2,3 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '.Customer.css';
 
+const Customer = (props) => {
+    return (
+        <div className="customer">
+            <span className="customer__content">
+                <div className="customer__content-name">{props.name}</div>
+                <div className="customer__content-created_at">{props.created_at}</div>
+                <div className="customer__content-address">{props.address}</div>
+                <div className="customer__content-pohone">{props.phone}</div>
+                <div className="customer__content-account_credit">{props.account_credit}</div>
+            </span>
+        </div>
+    )
+}
+
+Customer.propTypes = {
+
+};
+
+export default Customer;
