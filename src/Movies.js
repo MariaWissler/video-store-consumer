084d74daba1f07fea00b64fb45788eada6650139
Maria Wissler <wmcarmelina@gmail.com>
2019-06-26 11:21:35 -0700

    work on movie component

---
+++ src/Movies.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import './Movie.css';
+
+
+const Movie = (props) => {
+
+return (
+    <div className="movie">
+        <span className="movie__content">
+          <div className="movie__content-title">{props.title}</div>
+          <div className="movie__content-overview">{props.overview}</div>
+          <div className="movie__content-release_date">{props.release_date}</div>
+          <div className="movie__content-image_url"><img src={props.image_url} alt="new"/></div>
+        </span>
+      </div>
+)
+}
+    
+Movie.propTypes = {
+    id:PropTypes.integer,
+    title:PropTypes.string,
+    overview:PropTypes.string,
+    release_date:PropTypes.string,
+    image_url:PropTypes.string,
+    external_id:PropTypes.integer
+  };
+  
+  export default Movie;
