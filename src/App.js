09d6525b3b9ea925f4bf67f6e8014c090f915dd9
kateannnichols <kateannnichols@gmail.com>
2019-06-25 20:58:04 -0700

    added bootstrap, title, and logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import AppRouter from './Components/AppRouter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={"https://i.ibb.co/9pdVTpY/5a67a1b701d15068bdfe87c6.png"} className="App-logo" alt="Rilakkuma" />
+          <h1 className="App-title">Rilakkuma's Video Store</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro"> 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
